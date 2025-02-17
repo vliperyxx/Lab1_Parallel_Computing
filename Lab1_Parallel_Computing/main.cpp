663d60a091c49607e25f4c0f56a4453803463a70
vlady <vladysha1021@gmail.com>
2025-02-16 22:35:24 -0500

    Changed static array to vector

--- Lab1_Parallel_Computing/main.cpp
+++ Lab1_Parallel_Computing/main.cpp
@@ -6,27 +6,29 @@
 #include <vector>
 #include <algorithm>
 
-static const int SIZE = 20000;
-static int matrix[SIZE][SIZE];
 
 class MatrixOperations {
-public:
-    static void fillMatrix(int* matrix, int size) {
-        std::mt19937 rng(111);
-        std::uniform_int_distribution<int> dist(1, 10000);
+public:;
+      static void fillMatrix(std::vector<std::vector<int>>& matrix, int size) {
+          std::mt19937 rng(111);
+          std::uniform_int_distribution<int> dist(1, 10000);
 
-        for (int row = 0; row < size; row++) {
-            for (int col = 0; col < size; col++) {
-                matrix[row * size + col] = dist(rng);
-            }
-        }
-    }
+          for (int row = 0; row < size; row++) {
+              for (int col = 0; col < size; col++) {
+                  matrix[row][col] = dist(rng);
+              }
+          }
+      }
 };
 
 int main() {
     int size;
     std::cout << "Enter size of matrix: ";
     std::cin >> size;
+
+    std::vector<std::vector<int>> matrix(size, std::vector<int>(size));
+
+    MatrixOperations::fillMatrix(matrix, size);
 
     return 0;
 }
